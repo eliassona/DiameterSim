cca845a03ecb38a0fdc94267b0df8a3a5eb722af
Anders Eliasson <anders.eliasson@digitalroute.com>
2015-12-11 08:41:50 +0100

    updated url to github

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject diameter-sim "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Diameter simulator"
+  :url "https://github.com/eliassona/DiameterSim"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
